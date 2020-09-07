b890e74c73f7d25007e5f3b615ec6b807f3cd013
Seungjoon Woo <woo57@data.cs.purdue.edu>
2020-09-07 14:27:35 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,37 +4,37 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-
+rm Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv Memes/"Star Wars"/fafsa.jpg Memes/SpongeBob/fafsa.jpg
 
 # Task 4
-
+rm -R Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++/Age.cpp
 
 # Task 7
-
+cp Progams/"Java Homework"/Cs180/Project_1
 
 # Task 8
-
+mv Programs/Count.c Programs/Goodbye.c Programs/C
 
 # Task 9
-
+mv Programs/Name.py Programs/Space.py Programs/"Python 3"
 
 # Task 10
-
+touch Homework/Pol141/Essay.txt
 
 # Task 11
-
+pwd
 
 # Task 12
-
+set autoread autoindent number
